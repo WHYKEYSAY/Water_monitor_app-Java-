f7ee63627f1b8603eac6be83fbb59507a0bb5aa1
georgeHatem <ghy243@gmail.com>
2019-02-26 21:43:28 -0500

    Rebase

--- app/src/main/java/com/example/groupel/elecoen390_watermonitor/waterMonitordbHelper.java
+++ app/src/main/java/com/example/groupel/elecoen390_watermonitor/waterMonitordbHelper.java
@@ -1,6 +1,6 @@
 package com.example.groupel.elecoen390_watermonitor;
 
 public class waterMonitordbHelper {
+    //TODO: SQLite table definitions
     //TODO: Create, update, delete, get, getall for each table.
-    //TODO: Settings table is not important until we figure out what to put here.
 }
