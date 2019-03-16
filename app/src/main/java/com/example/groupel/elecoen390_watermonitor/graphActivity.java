13f31a379657f109e0c341f0b925627564f1a203
WilsonZiweiWang <wilsonwangziwei@gmail.com>
2019-03-16 17:30:37 -0400

    Graph features added (scrollable, zoomable) Bounds set. Labels added.

--- app/src/main/java/com/example/groupel/elecoen390_watermonitor/graphActivity.java
+++ app/src/main/java/com/example/groupel/elecoen390_watermonitor/graphActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.Date;
 import java.util.List;
 
 public class graphActivity extends AppCompatActivity {
@@ -20,28 +22,68 @@ public class graphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//display go-back arrow on top left
         Intent intent= getIntent();
         setUI();
         //TODO: add labels
         //TODO: add buttons that navigates to the detailed info
-        plot();
+        plot();//plotting takes info from respective item clicked at tableActivity
 
     }
 
     private void plot() {
         long wavelength = 0;
         long intensity = 0;
-        //Demo graph  y=sin(x)
+        int datapointcount = 50;
+
+        //Demo graph
         double x = 0;
         double y;
-        int datapointcount = 50;
         for (int i = 0; i < datapointcount; i++) {
-            x += 0.1;
-            y = Math.sin(x);
+            x += 1;
+            y = x;
             series.appendData(new DataPoint(x, y), true, 100);
         }
+
+        //map the datapoints
         graphView.addSeries(series);
 
+        // activate horizontal zooming and scrolling
+        graphView.getViewport().setScalable(true);
+
+        // activate horizontal scrolling
+        graphView.getViewport().setScrollable(true);
+
+        // activate horizontal and vertical zooming and scrolling
+        graphView.getViewport().setScalableY(true);
+
+        // activate vertical scrolling
+        graphView.getViewport().setScrollableY(true);
+
+        // set manual X bounds
+        //Note that the sensor will give 50 readings
+        graphView.getViewport().setXAxisBoundsManual(true);
+        graphView.getViewport().setMinX(0);
+        graphView.getViewport().setMaxX(datapointcount);
+
+        // set manual Y bounds
+        graphView.getViewport().setYAxisBoundsManual(true);
+        graphView.getViewport().setMinY(0);
+        graphView.getViewport().setMaxY(datapointcount);
+
+        // custom label formatter to show units
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // show um for x values
+                    return super.formatLabel(value, isValueX) + " um";
+                } else {
+                    // show um for y values
+                    return super.formatLabel(value, isValueX) + " um";
+                }
+            }
+        });
 
     }
 
