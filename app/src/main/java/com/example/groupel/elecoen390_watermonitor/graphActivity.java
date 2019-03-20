578aaeb6d7a281d310303665dbd8d9853e401b04
WilsonZiweiWang <wilsonwangziwei@gmail.com>
2019-03-20 10:37:56 -0400

    Codes revised

--- app/src/main/java/com/example/groupel/elecoen390_watermonitor/graphActivity.java
+++ app/src/main/java/com/example/groupel/elecoen390_watermonitor/graphActivity.java
@@ -25,7 +25,6 @@ public class graphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-        //TODO: go-back arrow not working, look it up
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//display go-back arrow on top left
         Intent intent= getIntent();
         setUI();
