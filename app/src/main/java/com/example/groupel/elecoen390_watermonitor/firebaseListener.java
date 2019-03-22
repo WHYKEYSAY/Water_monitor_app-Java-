7c6e1acb22b1a260157d81c775524570d9e04cd9
cottonswab <46750062+cottonswab@users.noreply.github.com>
2019-03-22 16:56:03 -0400

    Talal Michael Shuhaibar 2019-03-21 9:00 PM
    - Added Cloud Listener branch
    - Added cloudListener class
    - Set up Firebase database
    - Adjusted gradle files for Firebase functionality
    - Set up nodes (to be modified)
    - Attempted writing scrum methods
    - Commit contains errors
    
    Talal Michael Shuhaibar 2019-03-22 5:00 PM
    
    - Branched from master

---
+++ app/src/main/java/com/example/groupel/elecoen390_watermonitor/firebaseListener.java
@@ -0,0 +1,40 @@
+package com.example.groupel.elecoen390_watermonitor;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class cloudListener {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+    final DatabaseReference measure = myRef.child("measure");
+    final DatabaseReference position = myRef.child("position");
+    final DatabaseReference Vout = myRef.child("Vout");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Vout.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("file", "Value is: " + value);
+                //Do this
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+    }}
