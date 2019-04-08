3a2b5bfefaff3533c2faa5c06909f62ae1adce66
minhaoyu <46823793+minhaoyu@users.noreply.github.com>
2019-04-09 03:09:29 +0800

    change table

--- app/src/main/java/com/example/groupel/elecoen390_watermonitor/InfoTable.java
+++ app/src/main/java/com/example/groupel/elecoen390_watermonitor/InfoTable.java
@@ -2,15 +2,9 @@ package com.example.groupel.elecoen390_watermonitor;
 
 public class InfoTable {
   
-    String id,name,concentration,others;
+    String name;
 
-    public String getId(){
-        return id;
-    }
 
-    public void setId(String id){
-        this.id =id;
-    }
 
     public String getName(){
         return name;
@@ -20,20 +14,6 @@ public class InfoTable {
         this.name = name;
     }
 
-    public String getPercent(){
-        return concentration;
-    }
 
-    public void setPercent(String percent){
-        this.concentration = percent;
-    }
-
-    public String getOthers(){
-        return others;
-    }
-
-    public  void setOthers(String others){
-        this.others = others;
-    }
 }
 
